8322f9732a1620a4cab7c72c6da6005e23e98980
zk516 <yangpukun@gmail.com>
2016-04-14 00:22:28 +1200

    Create q7.c

---
+++ q7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap_ptr(int *p1,int *p2);
+int main(void)
+{
+    int p = 11, q = 22;
+    int *ptrp = &p, *ptrq = &q;
+    int **ppp = &ptrp, **ppq = &ptrq;
+    swap_ptr(ppp,ppq); /* &ptrp, &ptrq passed */
+    /* to swap_ptr() */
+    return 0;
+}
+void swap_ptr(int *p1, int *p2)
+{
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
