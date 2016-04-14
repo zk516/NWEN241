0871dd803b3b8466957a2da52d737337ea3437da
zk516 <yangpukun@gmail.com>
2016-04-14 20:42:01 +1200

    Update q8.c

--- q8.c
+++ q8.c
@@ -8,7 +8,8 @@ void average( int grades[][ EXAMS ], int pupils, int tests );
 void printArray( int grades[][ EXAMS ], int pupils, int tests );
 void printMenu( void );
 int main(void){
-    /* pointer to a function that takes as parameters a two-dimensional array and two integer values */
+    /* pointer to a function that takes as parameters a two-dimensional 
+array and two integer values */
     void ( *processGrades[ 4 ] )( int [][ EXAMS ], int, int )
     = { printArray, minimum, maximum, average};
     int choice = 0; /* menu choice */   /* array of student grades */
@@ -42,13 +43,29 @@ int main(void){
 
 /* search for the minimum value */
 void minimum( int grades[][ EXAMS ], int pupils, int tests ) {
-    int i = 0; /* loop counter */
-    int j = 0; /* loop counter */
-    int lowGrade = 100; /* set lowGrade to highest possible score */
-    for(i;i<pupils;i++){
-        for(j;j<tests;j++){
-            if(grades[i][j]<lowGrade){
-                lowGrade = grades[i][j];
+    // int i = 0; /* loop counter */
+    // int j = 0; /* loop counter */
+    // int lowGrade = 100; /* set lowGrade to highest possible score */
+    // for(i;i<pupils;i++){
+    //     for(j;j<EXAMS;j++){
+    //         if(((*grades+i)[j])<lowGrade){
+    //             lowGrade = (*grades+i)[j];
+    //             printf("lowGrade = %d and i = %d and j = %d\n",lowGrade,i,j);
+    //             printf("pupils = %d and tests = %d and EXAMS = %d\n",pupils,tests,EXAMS);
+    //         }
+    //     }
+    // }
+    int (*p1)[4] = grades;
+    int lowGrade = 100;
+
+    int *p2 = NULL;
+
+
+    for (; p1 != grades+3; ++p1) {
+        for (p2 = *p1; p2 != *p1+4; ++p2 ) {
+            if(*p2<lowGrade){
+                lowGrade = *p2;
+            //    printf("lowGrade = %d and i = %d and j = %d\n",lowGrade,i,j);
             }
         }
     }
@@ -60,14 +77,31 @@ void minimum( int grades[][ EXAMS ], int pupils, int tests ) {
 
 /* search for maximum value */
 void maximum( int grades[][ EXAMS ], int pupils, int tests ) {
-    int i = 0; /* loop counter */
-    int j = 0; /* loop counter */
-    int highGrade = 0; /* set highGrade to lowest possible score */
-    /* for you to complete */
-    for(i;i<pupils;i++){
-        for(j;j<tests;j++){
-            if(grades[i][j]>highGrade){
-                highGrade = grades[i][j];
+    // int i = 0; /* loop counter */
+    // int j = 0; /* loop counter */
+    // int highGrade = 0; /* set highGrade to lowest possible score */
+    // /* for you to complete */
+    // for(i;i<pupils;i++){
+    //     for(j;j<EXAMS;j++){
+    //         if(((*grades+j)[i])>highGrade){
+    //             highGrade = (*grades+j)[i];
+    //             printf("highGrade = %d and i = %d and j = %d\n",highGrade,i,j);
+    //             printf("pupils = %d and tests = %d and EXAMS = %d\n",pupils,tests,EXAMS);
+    //         }
+    //     }
+    // }
+    int highGrade = 0;
+    int (*p1)[4] = grades;
+
+
+    int *p2 = NULL;
+
+
+    for (; p1 != grades+3; ++p1) {
+        for (p2 = *p1; p2 != *p1+4; ++p2 ) {
+            if(*p2>highGrade){
+                highGrade = *p2;
+               // printf("lowGrade = %d and i = %d and j = %d\n",highGrade,i,j);
             }
         }
     }
@@ -103,7 +137,8 @@ void printMenu( void )
 {
 
     printf( "\n\tEnter a choice:\n"
-    "\t 0 Print the array of grades\n" "\t 1 Find the minimum grade\n" "\t 2 Find the maximum grade\n" "\t 3 Print the average on all"
+    "\t 0 Print the array of grades\n" "\t 1 Find the minimum grade\n" 
+"\t 2 Find the maximum grade\n" "\t 3 Print the average on all"
     " tests for each student\n"
     "\t 4 End program\n"
     "\t? " );
